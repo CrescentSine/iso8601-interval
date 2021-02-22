1a1d0fb461f9af562cbd285b329e46a234965a74
tianxiaoyang <tianxiaoyang@cxxmobi.com>
2021-02-22 17:09:05 +0800

    完成duration解析算法

--- src/duration.ts
+++ src/duration.ts
@@ -1,3 +1,5 @@
+import { strToTSA } from "./util";
+
 export interface Duration {
     readonly weeks: number;
     readonly wk: number;
@@ -92,13 +94,144 @@ class DurationImpl implements Duration {
     }
 }
 
+type TOKEN = string | number;
+type STATUS = (this: DurationProcessor, input: TOKEN) => STATUS;
+const DIGITS = "0123456789";
+const IS_DIGIT = /[0-9]/;
+const ZERO_CODE = '0'.charCodeAt(0);
+
+class DurationProcessor {
+    private _totalMS = 0;
+
+    getResultMS() {
+        return this._totalMS;
+    }
+
+    private _inputtingNum = 0;
+    private _inputtingDecimalUnit = 1;
+    private _signOfInputtingNum = 1;
+    private _nonIntegerInputted = false;
+    private _inputtedUnits = new Set();
+
+    private current: STATUS = this.init;
+
+    input(token: TOKEN) {
+        this.current = this.current(token);
+    }
+
+    private init(input: TOKEN): STATUS {
+        if (input === 'P') {
+            return this.waitingT;
+        }
+        throw new SyntaxError('The input must start with P');
+    }
+
+    private waitingT(input: TOKEN): STATUS {
+        if (input !== 'T') {
+            return this.waitingT;
+        }
+        return this.startInputNum;
+    }
+
+    private startInputNum(input: TOKEN): STATUS {
+        this._inputtingNum = 0;
+        this._nonIntegerInputted = false;
+        if (input === '+') {
+            this._signOfInputtingNum = +1;
+            return this.inputtingNum;
+        }
+        if (input === '-') {
+            this._signOfInputtingNum = -1;
+            return this.inputtingNum;
+        }
+        this._signOfInputtingNum = +1;
+
+        if (typeof input === 'number') {
+            this._inputtingNum = input;
+            return this.inputtingUnit;
+        }
+        if (input.match(IS_DIGIT)) {
+            return this.inputtingNum(input);
+        }
+        throw new SyntaxError('Must input a number before the unit sign');
+    }
+
+    private inputtingNum(input: TOKEN): STATUS {
+        if (input === '.') {
+            this._nonIntegerInputted = true;
+            this._inputtingDecimalUnit = 1;
+            return this.inputtingDecimalPart;
+        }
+        if (typeof input === 'number') {
+            throw new SyntaxError('Cannot combine templates and parameters as input number');
+        }
+        if (input.match(IS_DIGIT)) {
+            this._inputtingNum *= 10;
+            this._inputtingNum += input.charCodeAt(0) - ZERO_CODE;
+            return this.inputtingNum;
+        }
+        return this.inputtingUnit(input);
+    }
+
+    private inputtingDecimalPart(input: TOKEN): STATUS {
+        if (typeof input === 'number') {
+            throw new SyntaxError('Cannot combine templates and parameters as input number');
+        }
+        if (input.match(IS_DIGIT)) {
+            this._inputtingDecimalUnit /= 10;
+            this._inputtingNum += (input.charCodeAt(0) - ZERO_CODE) * this._inputtingDecimalUnit;
+            return this.inputtingDecimalPart;
+        }
+        return this.inputtingUnit(input);
+    }
+
+    private inputtingUnit(input: TOKEN): STATUS {
+        if (this._inputtedUnits.has(input)) {
+            throw new SyntaxError(`Cannot repeat input the unit "${input}"`);
+        }
+        const inputtedValue = this._inputtingNum * this._signOfInputtingNum;
+        switch (input) {
+            case 'H': this._totalMS += inputtedValue * MS_PER_HOUR; break;
+            case 'M': this._totalMS += inputtedValue * MS_PER_MINUTE; break;
+            case 'S': this._totalMS += inputtedValue * MS_PER_SECOND; break;
+            default: throw new SyntaxError(`Incorrect time unit "${input}" inputted`);
+        }
+        this._inputtedUnits.add(input);
+        // 因为浮点计算可能由精度误差，不要求模板参数为整数
+        if (input !== 'S' && this._nonIntegerInputted) {
+            throw new SyntaxError('Only seconds can be non-integer');
+        }
+        return this.startInputNum;
+    }
+}
+
 export function duration(ms: number): Duration;
 /** @param iso8601 PTnHnMnS */
 export function duration(iso8601: string): Duration;
 /** @param iso8601 PTnHnMnS */
 export function duration(iso8601: TemplateStringsArray, ...args: number[]): Duration;
 export function duration(input: number | string | TemplateStringsArray, ...args: number[]): Duration {
-    return new DurationImpl(0);
+    if (typeof input == 'number') {
+        return new DurationImpl(input);
+    }
+
+    let iso8601 = typeof input === "string" ? strToTSA(input) : input;
+    let processor = new DurationProcessor;
+
+    for (let i = 0; i < iso8601.length; ++i) {
+        for (let c of iso8601[i]) {
+            processor.input(c.toUpperCase());
+        }
+        if (i < args.length) {
+            if (typeof args[i] == 'number') {
+                processor.input(args[i]);
+            }
+            else {
+                throw new TypeError("The parameters of the template must be numerics");
+            }
+        }
+    }
+    return new DurationImpl(processor.getResultMS());
 }
 
 export namespace duration {
--- test/duration.test.ts
+++ test/duration.test.ts
@@ -1,6 +1,10 @@
 import { dur } from "../src";
 
-test("one seconds", function () {
+test('1000ms -> 1s', function () {
+    expect(dur(1000).s).toBe(1);
+});
+
+test("1s -> 1000ms", function () {
     expect(dur('PT1S').ms).toBe(1000);
-    expect(dur`PT${1}S`.ms).toBe(1000);
+    expect(dur`PT${1}S`.milliseconds).toBe(1000);
 });
