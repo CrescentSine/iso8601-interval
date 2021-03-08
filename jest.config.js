f4422bd6749daa02f0e1e6c295eaa2a322912439
tianxiaoyang <tianxiaoyang@cxxmobi.com>
2021-03-08 13:23:51 +0800

    使用coverallsapp/github-action，删除travis CI脚本

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
-  collectCoverage: true
+  testEnvironment: 'node'
 };
